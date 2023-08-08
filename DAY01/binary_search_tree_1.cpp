caf578d1f3b370b35ab3a535dd945bb7d55433a6
Tabi3a <abdell.rabiai@gmail.com>
2023-08-08 19:49:11 +0100

    THE ONE I PUSHED IN THE INTRA

---
+++ DAY01/binary_search_tree_1.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <algorithm>
+# include <stack>
+# include <string>
+# include <array>
+using namespace std;
+
+
+
+int main()
+{
+
+    return (0);
+}
---
+++ DAY02/A.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+# include <algorithm>
+# include <stack>
+# include <string>
+# include <array>
+// # include <bits/std++.h>
+# include <vector>
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    int q;
+    cin >> n;
+    cin >> q;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vector<long long> pre(n + 1, 0);
+
+    for (int i = 1; i <= n; i++)
+        pre[i] = pre[i - 1] + a[i - 1];
+
+    while (q--)
+    {
+        int l, r; cin >> l >> r;
+        cout  << pre[r] - pre[l - 1] << endl;
+    }
+    return (0);
+}
